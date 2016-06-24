39777a565b40ee5b9d99e891253040657b6f9c91
mehgellan <baumegg@gmail.com>
2016-06-24 12:00:00 -0700

    added if..else to game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,13 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+  alert("Sorry, try again.");
+}
+else if (cardThree === cardFour) {
+  alert("You found a match!");
+}
