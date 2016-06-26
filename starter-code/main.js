73db28215ee6d5e13c7c93cac8edd9eca8f19578
mehgellan <baumegg@gmail.com>
2016-06-26 15:37:27 -0700

    created DOM for game in JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,9 +5,27 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour) {
-  alert("Sorry, try again.");
+// if (cardTwo === cardFour) {
+//   alert("Sorry, try again.");
+// }
+// else if (cardThree === cardFour) {
+//   alert("You found a match!");
+// }
+
+var memoryBoard = document.getElementById('game-board');
+// create div for each card
+for (var i = 0; i < 4; i++) {
+  var newCard = document.createElement('div');
+  newCard.className = 'card';
+  memoryBoard.appendChild(newCard);
 }
-else if (cardThree === cardFour) {
-  alert("You found a match!");
-}
+
+// create function for HTML cards
+var createBoard = function() {
+  for (var i = 0; i < 4 ; i++) {
+    var newHTML = document.querySelector('.card');
+    newHTML.innerHTML = "<p>Hello</p>";
+    memoryBoard.appendChild(newHTML);
+  }
+};
+createBoard();
